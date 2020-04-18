3958ece81ba4b43f0959fe8ae5cda0ac5a6ef8a7
lgh <18520640132@163.com>
2020-04-19 01:24:12 +0800

    架构思考。 以插件化的方式支持多应用。因此去掉了角色、用户、企业管理的设计，并配置化应用管理。通过emqx+配置文件的方式通用化的解决设备接入编解码问题。 只保留：设备模型（配置文件）、编解码管理（配置文件）、设备管理、应用管理的核心功能设置。

--- iot-server/api/api.go
+++ iot-server/api/api.go
@@ -16,9 +16,19 @@ import (
 func GetServer() *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/api/v1/")
-	//{
-	//single interface
-	v1.GET("devicemodels", api.FindAllDeviceModels)
+
+	v1.GET("devicemodels", api.FindAllDeviceModels) //find all
+
+	v1.GET("device", nil)                          //find by ID
+	v1.GET("devices", nil)                         //find by Page
+	v1.POST("device", nil)                         //add
+	v1.DELETE("device", nil)                       //delete
+	v1.PUT("device", nil)                          //update
+	v1.POST("device/:deviceId/command/:uKey", nil) //send cmd
+
+	v1.GET("device/:deviceId/sensors", nil)  //find by date and deviceId and data number
+	v1.GET("device/:deviceId/alarms", nil)   //find by date and deviceId and data number
+	v1.GET("device/:deviceId/commands", nil) //find by date and deviceId and data number
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return r
